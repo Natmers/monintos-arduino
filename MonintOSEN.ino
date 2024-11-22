85349fab7cd77a3db03d0265d07ef474edea501f
YaroslavYath <98267444+Natmers@users.noreply.github.com>
2024-11-22 21:25:27 +0200

    Update MonintOSEN.ino
    Add new functions in Arduino-EN version

--- MonintOSEN.ino
+++ MonintOSEN.ino
@@ -19,25 +19,33 @@ void printDeviceInfo();
 void showCommandList();
 void printUptime();
 void checkMemory();
+void printUkrainianGameCompany();
+void STALKER();
+void monthInYear();
+void borsch();
 
 Command commands[] = {
     {"help", help},
     {"hello", printHello},
-    {"languages", printLanguages},
+    {"dish", borsch},
+    {"langs", printLanguages},
     {"year", printYear},
-    {"led_on", turnOnLED},
-    {"led_off", turnOffLED},
+    {"months", monthInYear},
+    {"game", printUkrainianGameCompany},
+    {"light", turnOnLED},
+    {"-light", turnOffLED},
     {"info", printInfo},
-    {"device", printDeviceInfo},
+    {"config", printDeviceInfo},
     {"uptime", printUptime},
     {"memory", checkMemory},
+    {"S.T.A.L.K.E.R.", STALKER},
     {"commands", showCommandList}
 };
 
 void setup() {
     Serial.begin(9600);
     systemStartTime = millis();
-    Serial.println(F("Welcome! I am the MonintOS system"));
+    Serial.println(F("Welcome! I am the MonintOS system."));
     pinMode(13, OUTPUT);
 }
 
@@ -62,43 +70,67 @@ void loop() {
 
 void help() {
     Serial.println(F(
-        "\nhello - greet the world\n"
-        "languages - Ukrainian programming languages: Mavka, Tziel\n"
+        "\nhello - greets the world\n"
+        "dish - national Ukrainian dish\n"
+        "langs - Ukrainian programming languages\n"
         "year - shows the current year\n"
+        "game - a small list of Ukrainian game dev companies\n"
+        "months - list of months in a year\n"
         "info - all information about the system and device\n"
-        "device - device specifications\n"
-        "led_on - turn on the built-in LED\n"
-        "led_off - turn off the built-in LED\n"
+        "config - device specifications\n"
+        "light - turns on the built-in LED\n"
+        "-light - turns off the built-in LED\n"
         "uptime - shows system uptime\n"
-        "memory - shows free memory"
+        "memory - shows free memory\n"
+        "S.T.A.L.K.E.R. - information about the S.T.A.L.K.E.R. game series"
     ));
 }
 
+void monthInYear() {
+    Serial.print(F("\n12 months in a year:\nJanuary, February, March, April, May, June, July, August, September, October, November, December"));
+}
+
 void printHello() {
-    Serial.println(F("\nHello, world!"));
+    Serial.println(F("\nHello, World!"));
 }
 
 void printLanguages() {
-    Serial.println(F("\nUkrainian programming languages: Mavka, Tziel"));
+    Serial.println(F("\nUkrainian programming languages - Mavka, Ziel"));
+}
+
+void borsch() {
+    Serial.print(F("\nThe national Ukrainian dish is borsch"));
 }
 
 void printYear() {
-    Serial.println(F("\nThe current year is 2024"));
+    Serial.println(F("\nIt is currently the year 2024"));
 }
 
 void turnOnLED() {
     digitalWrite(13, HIGH);
-    Serial.println(F("\nLED is on"));
+    Serial.println(F("\nLED is turned on"));
 }
 
 void turnOffLED() {
     digitalWrite(13, LOW);
-    Serial.println(F("\nLED is off"));
+    Serial.println(F("\nLED is turned off"));
+}
+
+void STALKER() {
+    Serial.println(F(
+        "\nS.T.A.L.K.E.R. game series:\n"
+        "Developer: GSC Game World\n"
+        "The first game in the series was created in 2007\n"
+        "The series currently consists of four games: S.T.A.L.K.E.R.: Shadow of Chernobyl, S.T.A.L.K.E.R.: Clear Sky, S.T.A.L.K.E.R.: Call of Pripyat, and the much-anticipated S.T.A.L.K.E.R. 2: Heart of Chernobyl\n"
+        "Games in the series are supported on Windows and Xbox\n"
+        "Additionally, the first three games were bundled in S.T.A.L.K.E.R.: Legends of the Zone Trilogy and are supported on PlayStation, Nintendo Switch, Xbox, and Windows\n"
+        "GSC Game World collaborated with NonStop to release energy drinks: orange S.T.A.L.K.E.R.: Limited Edition and purple S.T.A.L.K.E.R.: Limited Edition"
+    ));
 }
 
 void printInfo() {
     Serial.println(F(
-        "\nSystem Info:\n"
+        "\nSystem information:\n"
         "OS Name: MonintOS\n"
         "System Language: Ukrainian\n"
         "Device Name: Freenove V4\n"
@@ -106,19 +138,36 @@ void printInfo() {
         "Programming Language: C++\n"
         "Version: 0.0.0.1\n"
         "Release Date: 25.11.2024\n"
-        "Developer: Yatsenko Yaroslav"
+        "Developer: Yaroslav Yatsenko"
     ));
 }
 
 void printDeviceInfo() {
     Serial.println(F(
-        "\nDevice Info:\n"
+        "\nDevice specifications:\n"
         "Device: Freenove V4\n"
         "Processor: Atmega328P 16 MHz\n"
         "Flash Memory: 32 (31.5) KB\n"
         "RAM: 2 KB\n"
-        "EEPROM: 1 KB\n"
+        "Non-volatile Memory: 1 KB\n"
         "Compatible with Arduino UNO R3"
+    ));
+}
+
+void printUkrainianGameCompany() {
+    Serial.println(F(
+        "\nSome Ukrainian game development companies (list is expandable):\n"
+        "GSC Game World\n"
+        "4A Games\n"
+        "Best Way\n"
+        "Action Forms\n"
+        "N-Game Studios\n"
+        "Frogwares\n"
+        "PershaStudia\n"
+        "Brenntkopf Development\n"
+        "Muzt Die Studios\n"
+        "Frag Lab LLC\n"
+        "That's all for now, as memory constraints prevent further expansion."
     ));
 }
 
